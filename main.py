ea8292c71d7d142993abbd7140809753bba744db
Nicholas Reh <nicholas@Nicholass-MacBook-Air.local>
2022-12-30 12:50:47 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print(os.getenv("APP_ID"))
+
+headers = {
+    "x-app-id": os.getenv("APP_ID"),
+    "x-app-key": os.getenv("API_KEY"),
+    "Content-Type": "application/json",
+    "Authorization": os.getenv("SHEETY_AUTHORIZATION")
+}
+
+query = input("Tell me which exercises you did: ")
+
+post_params = {
+    "query": query
+}
+
+response = requests.post(url=os.getenv("EXERCISE_END_POINT"),
+                         json=post_params, headers=headers)
+data = response.json()
+
+print(data)
+
+for exercise in data["exercises"]:
+
+    date = datetime.now()
+
+    sheet_params = {
+        "workout": {
+            "date": date.strftime("%d/%m/%Y"),
+            "time": date.strftime("%X"),
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheety_response = requests.post(
+        url=os.getenv("SHEETY_POST_URL"), json=sheet_params, headers=headers)
+    print(sheety_response.text)
